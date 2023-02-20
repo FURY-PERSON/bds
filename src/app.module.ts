50f9904c35447024073605f26383f1c9bde9fe11
Михаил <78750882+FURY-PERSON@users.noreply.github.com>
2023-02-20 20:32:53 +0300

    add cross-env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
   controllers: [],
   providers: [],
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'cccv',
-      database: 'bds',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       entities: [],
       autoLoadEntities: true,
       synchronize: true,
     }),
+    UsersModule,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+  }
+}
