2067e82614be82336641426f2508c9796ad507ce
Михаил <78750882+FURY-PERSON@users.noreply.github.com>
2024-03-03 17:38:36 +0300

    add db seed

---
+++ seed.sql
@@ -0,0 +1,20 @@
+INSERT INTO role (name, description) VALUES
+    ('admin', 'admin role'),
+    ('student', 'student role'),
+    ('user', 'for all users'),
+    ('worker', 'worker role');
+
+
+
+INSERT INTO permissions (name, description) VALUES
+    ('admin', 'admin role'),
+    ('student', 'student role'),
+    ('user', 'for all users'),
+    ('worker', 'worker role');
+
+
+INSERT INTO featureFlagList (name) VALUES
+    ('counter'),
+    ('newDesign'),
+    ('ratingCardOnMainPage'),
+    ('test');
